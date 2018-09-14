edb9dd881165588974029d3541cc6e0d428640a5
Mio Engel <mioengel@gmail.com>
2018-09-14 12:50:08 +0200

    Adds webpack with babel and scss loaders

---
+++ web/src/js/index.js
@@ -0,0 +1,12 @@
+import '../sass/common.scss';
+import '../sass/content.scss';
+import '../sass/footer.scss';
+import '../sass/glass.scss';
+import '../sass/header.scss';
+import '../sass/navigation.scss';
+import '../sass/themes/standard.scss';
+
+const foo = () => { console.log('bar'); };
+
+foo();
+
