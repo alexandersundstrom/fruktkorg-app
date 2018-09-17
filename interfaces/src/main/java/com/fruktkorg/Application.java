cf6a9a876a7d6d7bfe9ceee09ae28424ab2fe90c
Alexander Sundström <alexander.sundstrom@dynabyte.se>
2018-09-17 12:13:42 +0200

    Adds CORS configuration and changes server port

--- interfaces/src/main/java/com/fruktkorg/Application.java
+++ interfaces/src/main/java/com/fruktkorg/Application.java
@@ -3,18 +3,33 @@ package com.fruktkorg;
 import com.fruktkorg.security.AuthenticationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.servlet.Filter;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
+
     }
 
     @Bean(name = "authenticationFilter")
     public Filter getAuthenticationFilter() {
         return new AuthenticationFilter();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").allowedOrigins("http://localhost:8080", "http://localhost:8090");
+            }
+        };
     }
 }
