bf0c6b8dc6a08b012d9ebd10ad1907c4f3ebdaf0
Alexander Sundström <alexander.sundstrom@dynabyte.se>
2018-09-25 15:54:33 +0200

    Refactors Test class

--- interfaces/src/test/java/ChromeTest.java
+++ interfaces/src/test/java/ChromeTest.java
@@ -22,39 +22,29 @@ import java.util.List;
 public class ChromeTest {
 
     private static WebDriver driver;
-    private static BrowserMobProxy proxy;
-    private static final String FIRST_ROW_AND_FIRST_COLUMN = "//tr[1]/td[1]";
+    private final String BASE_URL = "http://localhost:8080";
+    private static final String SCREENSHOT_FOLDER = "Test Screenshots";
 
-    private final String ID_PAGINATION_CONTAINER = "pagination-container";
-
+    //PAGINATION FIELDS
     private WebElement firstPageButton;
-    private final String ID_FIRST_PAGE_BUTTON = "firstPageButton";
-
     private WebElement previousPageButton;
-    private final String ID_PREVIOUS_PAGE_BUTTON = "previousPageButton";
-
     private WebElement nextPageButton;
-    private final String ID_LAST_PAGE_BUTTON = "lastPageButton";
-
     private WebElement lastPageButton;
-    private final String ID_NEXT_PAGE_BUTTON = "nextPageButton";
 
     private Select limitSelector;
-    private final String ID_LIMIT_SELECTOR = "limitSelector";
 
     private WebElement pageInfo;
     private final String ID_PAGEINFO = "pageInfo";
 
+    //TABLE FIELDS
     private WebElement table;
     private final String ID_TABLE = "full-width-table";
+    private static final String XPATH_FIRST_ROW_AND_FIRST_COLUMN = "//tr[1]/td[1]";
 
+    //OTHER FIELDS
     private final String ID_SEARCH_FRUKT_TAB = "menu_searchfrukt";
     private final String ID_TEXTFIELD_SEARCH = "search";
-    private final String ID_NAME_TOGGLE = "sort-by-name";
-    private final String ID_THEME_SELECTOR = "theme-selector";
-    private final String BASE_URL = "http://localhost:8080";
 
-    private static final String SCREENSHOT_FOLDER = "Test Screenshots";
 
     @Rule
     public TestWatcher screenshotRule = new TestWatcher() {
@@ -87,7 +77,7 @@ public class ChromeTest {
         }
 
         // start the proxy
-        proxy = new BrowserMobProxyServer();
+        BrowserMobProxy proxy = new BrowserMobProxyServer();
         proxy.start(0);
         proxy.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT, CaptureType.RESPONSE_CONTENT);
         proxy.addHeader("X-PERSONR", "19880301-1234");
@@ -115,14 +105,15 @@ public class ChromeTest {
 
         waitAndGetElementById(ID_TEXTFIELD_SEARCH).sendKeys("Päron" + Keys.ENTER);
 
-        WebElement table = waitAndGetElementByClassName(ID_TABLE);
-        waitForTextByXPath(FIRST_ROW_AND_FIRST_COLUMN, "Kafferummet 101");
+        table = waitAndGetElementByClassName(ID_TABLE);
+        waitForTextByXPath(XPATH_FIRST_ROW_AND_FIRST_COLUMN, "Kafferummet 101");
 
         List rows = table.findElements(By.tagName("tr"));
         Assert.assertEquals(10, rows.size());
 
+        String ID_NAME_TOGGLE = "sort-by-name";
         waitAndGetElementById(ID_NAME_TOGGLE).click();
-        waitForTextByXPath(FIRST_ROW_AND_FIRST_COLUMN, "96");
+        waitForTextByXPath(XPATH_FIRST_ROW_AND_FIRST_COLUMN, "96");
     }
 
     @Test
@@ -230,8 +221,10 @@ public class ChromeTest {
         waitAndGetElementById(ID_TEXTFIELD_SEARCH)
                 .sendKeys("Jordgubbar" + Keys.ENTER);
 
-        Assert.assertFalse(isPresent(ID_TABLE));
-        Assert.assertFalse(isPresent(ID_PAGINATION_CONTAINER));
+        Assert.assertFalse(isIdPresent(ID_TABLE));
+        //PAGINATION ELEMENTS
+        String ID_PAGINATION_CONTAINER = "pagination-container";
+        Assert.assertFalse(isIdPresent(ID_PAGINATION_CONTAINER));
 
         WebElement noItemsTextElement = waitAndGetElementByClassName("no-items");
         Assert.assertEquals("Inga fruktkorgar hittade.", noItemsTextElement.getText());
@@ -240,12 +233,12 @@ public class ChromeTest {
     @Test
     public void changeTheme() {
         driver.get(BASE_URL);
-        waitAndGetElementByClassName("theme-standard");
+        Assert.assertTrue(isClassPresent("theme-standard"));
 
-        Select themeSelector = getSelect(ID_THEME_SELECTOR);
+        Select themeSelector = getSelect("theme-selector");
         themeSelector.selectByValue("theme-high-contrast");
 
-        waitAndGetElementByClassName("theme-high-contrast");
+        Assert.assertTrue(isClassPresent("theme-high-contrast"));
 
     }
 
@@ -270,8 +263,12 @@ public class ChromeTest {
 
     }
 
-    private boolean isPresent(String id) {
+    private boolean isIdPresent(String id) {
         return driver.findElements(By.id(id)).size() > 0;
+    }
+
+    private boolean isClassPresent(String className) {
+        return driver.findElements(By.className(className)).size() > 0;
     }
 
     private void waitForTextByXPath(String path, String text) {
@@ -288,11 +285,11 @@ public class ChromeTest {
     }
 
     private void getAllPaginationElements() {
-        this.firstPageButton = waitAndGetElementById(ID_FIRST_PAGE_BUTTON);
-        this.previousPageButton = waitAndGetElementById(ID_PREVIOUS_PAGE_BUTTON);
-        this.lastPageButton = waitAndGetElementById(ID_LAST_PAGE_BUTTON);
-        this.nextPageButton = waitAndGetElementById(ID_NEXT_PAGE_BUTTON);
-        this.limitSelector = getSelect(ID_LIMIT_SELECTOR);
+        this.firstPageButton = waitAndGetElementById("firstPageButton");
+        this.previousPageButton = waitAndGetElementById("previousPageButton");
+        this.lastPageButton = waitAndGetElementById("lastPageButton");
+        this.nextPageButton = waitAndGetElementById("nextPageButton");
+        this.limitSelector = getSelect("limitSelector");
         this.pageInfo = waitAndGetElementById(ID_PAGEINFO);
     }
 }
